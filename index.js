31f1915b10b463138e13f2eeeb5778e1d5295543
viswanadh7 <mudunuruviswanadh@gmail.com>
2024-07-01 17:05:04 +0530

    Added Authorization

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ const mongoose = require("mongoose");
 const userRouter = require("./routes/userRouter");
 const postRouter = require("./routes/postRouter");
 const MONGO_URL = process.env.MONGO_URL;
+
+const apiKeyAuth = (req, res, next) => {
+    const apiKey = req.headers.authorization;
+    if (!apiKey || apiKey !== `Bearer ${process.env.API_KEY}`) {
+        return res.status(403).json({ error: "Unauthorized" });
+    }
+    next();
+};
 mongoose
     .connect(MONGO_URL)
     .then(() => console.log("DataBase connected"))
@@ -12,6 +20,7 @@ const cors = require("cors");
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(apiKeyAuth);
 //Routing
 app.use(userRouter);
 app.use(postRouter);
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,10 +1,31 @@
 const express = require("express");
 const router = express.Router();
 const userModel = require("../models/userSchema");
-
-router.get("/users", async (req, res) => {
-    const allUsers = await userModel.find();
-    res.json(allUsers);
+const bcrypt = require("bcrypt");
+// router.get("/users", async (req, res) => {
+//     const allUsers = await userModel.find();
+//     res.json(allUsers);
+// });
+router.post("/login", async (req, res) => {
+    const { email, password } = req.body;
+    await userModel
+        .findOne({ email: email })
+        .then((user) => {
+            if (user.password === password) {
+                console.log("Matched");
+                const sendingUser = {
+                    id: user._id,
+                    firstname: user.firstname,
+                    profile_url: user.profile_url,
+                };
+                res.status(200).send(sendingUser);
+            } else {
+                res.status(404).send("Incorrect password. Please try again");
+            }
+        })
+        .catch((error) => {
+            res.status(404).send("User not found. Please create an account");
+        });
 });
 router.get("/users/:id", async (req, res) => {
     const userById = await userModel.findById(req.params.id);
